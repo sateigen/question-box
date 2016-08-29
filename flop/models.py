ddd27f1c82c9469486a16ac92785f5fd0a4782ee
mendez6412 <mendez6412@gmail.com>
2016-08-29 17:21:42 -0400

    updated models, need migrations

--- flop/models.py
+++ flop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Question(models.Model):
+    user = models.ForeignKey(User)
+    score = models.IntegerField()
+    created_on = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    selected_answer = models.OneToOneField('Answer', null=True)
+    tags = models.ManyToManyField(Tag)
+    rated_by = models.ManyToManyField(User)
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User)
+    score = models.IntegerField()
+    created_on = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    question = models.ForeignKey(Question)
+    rated_by = models.ManyToManyField(User)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User)
+    created_on = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    answer = models.ForeignKey(Answer, null=True)
+    question = models.ForeignKey(Question, null=True)
--- question_box/settings.py
+++ question_box/settings.py
@@ -18,6 +18,8 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'flop.apps.FlopConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
