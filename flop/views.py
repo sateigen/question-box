9192415bb85d5818945c40df6e8e82213e370f04
B. Maxwell Ruotsi <bmruotsi@gmail.com>
2016-08-30 13:54:14 -0400

    simple templates made, login, logout, and registration added to views

--- flop/views.py
+++ flop/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST, prefix='user')
+        if user_form.is_valid():
+            user = user_form.save(commit=False)
+            user.save()
+            return HttpResponseRedirect('/')
+    else:
+        user_form = UserCreationForm(prefix='user')
+    context = {'userform': user_form}
+    return render(request, 'flop/register.html', context)
+
+
+def signin(request):
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/')
+    return render(request, 'flop/login.html')
+
+
+def signout(request):
+    if request.POST:
+        return logout(request, request.POST["logout"])
+    else:
+        logout(request)
+        return render(request, 'flop/logout.html')
