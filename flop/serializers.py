9da6ce12435c13848269b2139274068723f81d98
mendez6412 <mendez6412@gmail.com>
2016-08-30 13:51:23 -0400

    started serializers

---
+++ flop/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Question, Answer, Comment, Tag
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email')
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('id', 'user', 'score', 'created_on', 'title', 'description', 'selected_answer', 'tags', 'rated_by')
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ('id', 'user', 'score', 'created_on', 'description', 'question', 'rated_by')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'user', 'created_on', 'description', 'question', 'answer')
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
--- flop/views.py
+++ flop/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Question, Answer, Comment, Tag
+from django.contrib.auth.models import User
+from .serializers import QuestionSerializer, AnswerSerializer, CommentSerializer, TagSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    serializer_class = QuestionSerializer
+    queryset = Question.objects.all()
+
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    serializer_class = AnswerSerializer
+    queryset = Answer.objects.all()
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
