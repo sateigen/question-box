ee1d497c5d86c5d330999ae5e891697d986091e5
Peter Lawless <peterlawless223@gmail.com>
2016-08-31 06:51:05 -0400

    Fixed registration view to hash passwords

--- flop/forms.py
+++ flop/forms.py
@@ -3,7 +3,6 @@ from django import forms
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
     first_name = forms.CharField(max_length=20)
     last_name = forms.CharField(max_length=20)
 
